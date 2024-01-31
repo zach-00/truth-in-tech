3c378dea35094778a7b0e76fd8dc7ce3553eb10e
toonwodrld2102 <suanadetunji@yahoo.com>
2024-01-31 20:07:42 +0000

    Resolve "Create-a-Company/#28"

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,20 +2,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import "./App.css";
 import LoginForm from "./login.js";
+import CreateCompanyForm from "./CreateCompany.js";
 
 function App() {
+  const basename=process.env.PUBLIC_HOST
+  const baseUrl=process.env.REACT_APP_API_HOST
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
     <div className="container">
-        <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}>
+      <AuthProvider baseUrl={baseUrl}>
 
-      <Routes>
-        <Route index element={<LoginForm />} />
-        {/* <Route path="/reviews"></Route> */}
+        <Routes>
+          <Route path="login" element={<LoginForm />} />
+          {/* <Route path="/reviews"></Route> */}
+          <Route path="companies">
+            <Route path="create" element={<CreateCompanyForm/>}/>
+          </Route>
+        </Routes>
 
-      </Routes>
-
-        </AuthProvider>
+      </AuthProvider>
     </div>
     </BrowserRouter>
   );
---
+++ ghi/src/CreateCompany.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { useAuthContext } from '@galvanize-inc/jwtdown-for-react';
+
+const CreateCompanyForm = () => {
+  const [companyName, setCompanyName] = useState('');
+  const [companyLogo, setCompanyLogo] = useState('');
+  const { token } = useAuthContext()
+
+    const handleNameChange = (event) => {
+        const value = event.target.value;
+        setCompanyName(value);
+    }
+
+    const handleLogoChange = (event) => {
+        const value = event.target.value;
+        setCompanyLogo(value);
+    }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+        const response = await fetch(`${process.env.REACT_APP_API_HOST}/companies`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            company_name: companyName,
+            company_logo: companyLogo,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+        if (response.ok){
+            const data = await response.json();
+            console.log('Company created:', data);
+
+            setCompanyName('');
+            setCompanyLogo('')
+        }
+
+    } catch (error) {
+      console.error('Error creating company:', error.message);
+    }
+  };
+
+  return (
+    <form className="was-validated" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="companyName" className="form-label">
+          Company Name:
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="companyName"
+          value={companyName}
+          onChange={handleNameChange}
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="companyLogo" className="form-label">
+          Company Logo:
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="companyLogo"
+          value={companyLogo}
+          onChange={handleLogoChange}
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <button type="submit" className="btn btn-primary" >
+          Submit form
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default CreateCompanyForm;
