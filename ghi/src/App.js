4545bfd1eece6c18c3bfca976889bc40bcd83bcc
zach-00 <walkowiakzach05@gmail.com>
2024-02-01 00:45:16 +0000

    Create review/#30

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -3,6 +3,7 @@ import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import "./App.css";
 import LoginForm from "./login.js";
 import CreateCompanyForm from "./CreateCompany.js";
+import CreateReview from "./CreateReview.js";
 
 function App() {
   const basename=process.env.PUBLIC_HOST
@@ -12,13 +13,18 @@ function App() {
     <div className="container">
       <AuthProvider baseUrl={baseUrl}>
 
-        <Routes>
-          <Route path="login" element={<LoginForm />} />
-          {/* <Route path="/reviews"></Route> */}
-          <Route path="companies">
-            <Route path="create" element={<CreateCompanyForm/>}/>
-          </Route>
-        </Routes>
+      <Routes>
+        <Route path="login" element={<LoginForm />} />
+
+        <Route path="companies">
+          <Route path="create" element={<CreateCompanyForm/>}/>
+        </Route>
+
+        <Route path="reviews">
+          <Route path="create" element={<CreateReview />} />
+        </Route>
+
+      </Routes>
 
       </AuthProvider>
     </div>
