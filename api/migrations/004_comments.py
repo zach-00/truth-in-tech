994df2116008b5acba321fe1b9942ac344bc1ad2
toonwodrld2102 <suanadetunji@yahoo.com>
2024-02-07 21:01:15 +0000

    Resolve "CreateCommentsTable"

---
+++ api/migrations/004_comments.py
@@ -0,0 +1,16 @@
+steps = [
+    [
+    """
+    CREATE TABLE comments(
+    id SERIAL PRIMARY KEY NOT NULL,
+    body TEXT,
+    account_id INTEGER REFERENCES accounts(id),
+    review_id INTEGER REFERENCES reviews(id),
+    date_created TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP
+    );
+    """,
+    """
+    DROP TABLE comments;
+    """,
+    ],
+]
