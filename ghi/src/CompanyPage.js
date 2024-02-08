08f6cbc2cd87a85cea8040900916fbc23bf1cec1
Cory Egan <cory.egan@gmail.com>
2024-02-08 00:32:01 +0000

    Search bar company page

--- ghi/src/CompanyPage.js
+++ ghi/src/CompanyPage.js
@@ -11,8 +11,9 @@ import { Link } from "react-router-dom";
 function CompanyPage() {
   const [reviews, setReviews] = useState([]);
   const { id } = useParams();
+  const [searchQuery, setSearchQuery] = useState("");
 
-    useEffect(() => {
+  useEffect(() => {
     async function getReviews() {
       const url = `${process.env.REACT_APP_API_HOST}/reviews/companies/${id}`;
       const response = await fetch(url);
@@ -33,48 +34,93 @@ function CompanyPage() {
             "https://upload.wikimedia.org/wikipedia/commons/3/37/Sad-face.png",
         };
 
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value.toLowerCase());
+  };
+
+  const filteredReviews = reviews.filter((review) => {
+    const body = review.body.toLowerCase();
+    const bodySearch = body.includes(searchQuery)
+    const username = review.username.toLowerCase();
+    const usernameSearch = username.includes(searchQuery);
+    const location = review.location.toLowerCase();
+    const locationSearch = location.includes(searchQuery);
+    const jobtitle = review.job_title.toLowerCase();
+    const jobtitleSearch = jobtitle.includes(searchQuery);
+    const salary = review.salary.toString();
+    const salarySearch = salary.includes(searchQuery);
+
+    if(bodySearch) {
+      return true;
+    }
+    if(usernameSearch && review.anonymous === false) {
+      return true
+    ;
+    }
+    if(locationSearch) {
+      return true;
+    }
+    if(jobtitleSearch) {
+      return true;
+    }
+    if(salarySearch) {
+      return true;
+    }
+
+    return false;
+  });
+
   return (
     <>
-    <div className="row">
-      <div className="offset-2 col-6">
-      <div>
-        <h1>
-          {companyInfo.company_name} <br></br>{" "}
-          <Badge bg="secondary">Reviews</Badge>
-        </h1>
-      </div>
-      <div>
-        <br></br>
-        <Container>
-          <Row>
-            <Col xs={6} md={4}>
-              <Image src={`${companyInfo.company_logo}`} thumbnail />
-            </Col>
-          </Row>
-        </Container>
-        <br></br>
-        {reviews.map((review) => (
-          <div key={review.id}>
-            <Card style={{ width: "64rem" }}>
-              <Card.Body>
-                <Card.Title>
-                  {review.anonymous === true ? "" : `${review.username}`} $
-                  {review.salary}/yr {review.job_title}
-                </Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">
-                  {review.location}
-                </Card.Subtitle>
-                <Card.Text>{review.body}</Card.Text>
-                <Card.Link as={Link} to={`/review/${review.id}`}>
-                  Check this review out
-                </Card.Link>
-              </Card.Body>
-            </Card>
-            <br></br>
+      <div className="row">
+        <div className="offset-2 col-6">
+          <div>
+            <h1>
+              {companyInfo.company_name} <br></br>{" "}
+              <Badge bg="secondary">Reviews</Badge>
+            </h1>
           </div>
-        ))}
-      </div>
-      </div>
+            <div className="input-group">
+                <input
+                  type="search"
+                  placeholder="Search (use lowercase)..."
+                  className="form-control"
+                  value={searchQuery}
+                  onChange={handleSearchInputChange}
+                />
+            </div>
+          <div>
+            <br></br>
+            <Container>
+              <Row>
+                <Col xs={6} md={4}>
+                  <Image src={`${companyInfo.company_logo}`} thumbnail />
+                </Col>
+              </Row>
+            </Container>
+            <br></br>
+            {filteredReviews.map((review) => (
+              <div key={review.id}>
+                <Card style={{ width: "64rem" }}>
+                  <Card.Body>
+                    <Card.Title>
+                      {review.anonymous === true ? "" : `${review.username}`} $
+                      {review.salary}/yr {review.job_title}
+                    </Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      {review.location}
+                    </Card.Subtitle>
+                    <Card.Text>{review.body}</Card.Text>
+                    <Card.Link as={Link} to={`/review/${review.id}`}>
+                      Check this review out
+                    </Card.Link>
+                  </Card.Body>
+                </Card>
+                <br></br>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
