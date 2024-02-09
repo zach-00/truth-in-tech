c9454d99a5ed98048b40697b07ca5bb276a69e26
Zach <walkowiakzach05@gmail.com>
2024-02-08 19:44:57 -0800

    Removed console.log statement

--- ghi/src/CompanyPage.js
+++ ghi/src/CompanyPage.js
@@ -19,7 +19,6 @@ function CompanyPage() {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data)
         setReviews(data);
       }
     }
